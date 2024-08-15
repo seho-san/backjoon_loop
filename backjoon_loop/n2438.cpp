a0993b7431728643dd24a8a38667ff90690e9154
Seho_Si <sunnyseho01@gmail.com>
2024-08-15 20:45:49 +0900

    * 개수 실수. 줄과 별의 갯수가 매칭이 되지 않았음. 수정 완.

---
+++ backjoon_loop/n2438.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i < n+1; i++) {
+		for (int j = 0; j < i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
