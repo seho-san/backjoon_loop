fd50f7da861461d23b2d9b6ad61fa076c634f259
Seho_Si <sunnyseho01@gmail.com>
2024-08-15 15:16:32 +0900

    금액 검증

---
+++ backjoon_loop/n25304.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int total,n;
+	scanf("%d", &total);
+	scanf("%d", &n);
+
+	int a, b;
+	int c = 0;
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &a, &b);
+		c += (a * b);
+	}
+	if (c == total)
+		printf("Yes");
+	else
+		printf("No");
+}
---
+++ backjoon_loop/n8393.cpp
@@ -0,0 +1,13 @@
+//#include<stdio.h>
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//int main() {
+//	int n,i;
+//	scanf("%d", &n);
+//	int result=0;
+//	for (i = 1; i < n+1; i++) {
+//		result += i;
+//	}
+//	printf("%d", result);
+//	return 0;
+//}
