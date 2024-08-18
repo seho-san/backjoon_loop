1ce2a3c2b2b480aa5d054f2b2bc4178205ae9169
Seho_Si <sunnyseho01@gmail.com>
2024-08-18 21:45:34 +0900

    break 사용 놓침

--- backjoon_loop/n2439.cpp
+++ backjoon_loop/n2439.cpp
@@ -1,21 +1,16 @@
-#include <stdio.h>
-
-
-int main() {
-
-	int t;
-	scanf("%d", &t);
-
-	for (int i = 1; i <= t; i++) {
-		for (int k = 0; k < t - i; k++) {
-			printf(" ");
-		}
-
-		for (int k = 0; k < i; k++) {
-			printf("*");
-		}
-		printf("\n");
-	}
-
-	return 0;
-}
+//#include <stdio.h>
+//
+//int main() {
+//	int t;
+//	scanf("%d", &t);
+//	for (int i = 1; i <= t; i++) {
+//		for (int k = 0; k < t - i; k++) {
+//			printf(" ");
+//		}
+//		for (int k = 0; k < i; k++) {
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
